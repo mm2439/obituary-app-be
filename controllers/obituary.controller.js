c59a88bf20193befa0c23b2ebec9418d372c4074
Mohsin Zafar <heykhattak@gmail.com>
2025-06-25 10:53:56 +0500

    slug key for obituary fixed

--- controllers/obituary.controller.js
+++ controllers/obituary.controller.js
@@ -60,13 +60,10 @@ const obituaryController = {
           const d = new Date(date);
           const day = String(d.getDate()).padStart(2, "0");
           const month = String(d.getMonth() + 1).padStart(2, "0");
-          const year = d.getFullYear();
-          return `${day}.${month}.${year}`;
+          const year = String(d.getFullYear()).slice(-2);
+          return `${day}${month}${year}`;
         };
-        slugKey = `${name}_${sirName}_${formatDate(deathDate)}`.replace(
-          /\s+/g,
-          "_"
-        );
+        slugKey = `${name}_${sirName}_${formatDate(deathDate)}`;
       }
       // Ensure slugKey is unique, append number if needed
       let uniqueSlugKey = slugKey;
