0e0376c1aa97ee32671535d0fa5ce58745554489
syed ali shah <ali.web009@gmail.com>
2025-07-29 15:12:19 +0500

    fix obituary permation issue

--- middlewares/checkPermission.js
+++ middlewares/checkPermission.js
@@ -3,7 +3,6 @@ const { Request, Response, NextFunction } = require("express");
 const checkPermission = (permissionKey) => {
   return (req, res, next) => {
     const user = req.user;
-
     if (!user || !user[permissionKey]) {
       return res.status(403).json({ message: "Permission denied." });
     }
