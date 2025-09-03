b407e6bb76fd6dbd63400175f46063f0b3316b62
jaskaran26 <jasskaranofficial@gmail.com>
2025-09-03 18:10:56 +0530

    added new col

---
+++ migrations/20250903113135-add-new-status-to-companypages.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      ALTER TYPE "enum_companypages_status"
+      ADD VALUE IF NOT EXISTS 'SENT_FOR_APPROVAL';
+    `);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
