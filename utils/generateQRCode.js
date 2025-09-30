ce8ef1b8f111cf0a2553b95f87511b89cf6e6134
jaskaran26 <jasskaranofficial@gmail.com>
2025-09-30 18:03:13 +0530

    changes

--- utils/generateQRCode.js
+++ utils/generateQRCode.js
@@ -11,8 +11,6 @@ export async function generateQRCode(data, id) {
             body: 'circle',
             eye: 'frame1',
             eyeBall: 'ball0',
-            // You can customize colors/logos here
-            // For example, set color dark/light
             color: {
                 dark: '#000000',
                 light: '#FFFFFF'
@@ -31,7 +29,7 @@ export async function generateQRCode(data, id) {
             headers: {
                 'Content-Type': 'application/json',
             },
-            responseType: 'arraybuffer', // important to get image buffer
+            responseType: 'arraybuffer', 
         });
         const remotePath = buildRemotePath(
             "qr-code",
@@ -50,55 +48,3 @@ export async function generateQRCode(data, id) {
         console.error('Error generating QR code:', error.response?.data || error.message);
     }
 }
-
-
-
-// export async function generateQRCode(data, id) {
-//     const url = 'https://www.qrcode-monkey.com/qr/custom';
-
-//     const postData = {
-//         data: data,
-//         config: {
-//             body: 'circle',
-//             eye: 'frame1',
-//             eyeBall: 'ball0',
-//             // You can customize colors/logos here
-//             // For example, set color dark/light
-//             color: {
-//                 dark: '#000000',
-//                 light: '#FFFFFF'
-//             },
-//             // Logo config, optional
-//             logo: 'https://osmrtnica.com/omr.png',
-//             logoMode: 'clean',
-//         },
-//         size: 300,
-//         download: false, // Set to true if you want direct download
-//         file: 'png',
-//     };
-
-//     try {
-//         const response = await axios.post(url, postData, {
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             responseType: 'arraybuffer', // important to get image buffer
-//         });
-//         const remotePath = buildRemotePath(
-//             "qr-code",
-//             `${String(id)}.png`,
-//         );
-//         console.log('response', response);
-
-//         await uploadBuffer(
-//             response.data,
-//             remotePath,
-//             "image/png"
-//         );
-//         return publicUrl(remotePath);
-
-
-//     } catch (error) {
-//         console.error('Error generating QR code:', error.response?.data || error.message);
-//     }
-// }
