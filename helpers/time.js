8b88f605acc4f1ad4527a91bf798ec3726537a00
sibshahz <shahid5ssg@gmail.com>
2026-02-15 23:02:38 +0500

    fixed time format to dots

--- helpers/time.js
+++ helpers/time.js
@@ -2,7 +2,7 @@ const formatDDMMYYYY = (date) => {
   const dd = String(date.getDate()).padStart(2, "0");
   const mm = String(date.getMonth() + 1).padStart(2, "0"); // months are 0-based
   const yyyy = date.getFullYear();
-  return `${dd}-${mm}-${yyyy}`;
+  return `${dd}.${mm}.${yyyy}`;
 };
 
 module.exports = { formatDDMMYYYY };
